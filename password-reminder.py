f11dce93afda3e641c578b44cbcb2255ae28cd43
tubasaf <77580305+tubasaf@users.noreply.github.com>
2021-06-07 23:45:35 +0200

    password reminder

---
+++ password-reminder.py
@@ -0,0 +1,7 @@
+user_name = "Joseph"
+user = input("Please enter your name:")
+if user == user_name:
+    print("Hello Joseph! The password is:W@12")
+else:
+    print("Hello {}, See you later".format (user) )
+
